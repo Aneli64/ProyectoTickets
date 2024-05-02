ac9a8c8c9dae33d6e7105a904958944e88bb093c
Elian <eliandeniz27@gmail.com>
2024-05-02 09:30:26 +0200

    codigo corregido

--- infoApi.js
+++ infoApi.js
@@ -1,43 +1,40 @@
-// const axios = require('axios');
+const https = require('https');
 
-// const token = '55e32b85-8a30-4533-84a0-9f5b1b81f45f';
-// const apiUrl = 'https://app.na5.teamsupport.com/api/json/customers';
+const organizationID = '748448';
+const apiToken = '55e32b85-8a30-4533-84a0-9f5b1b81f45f';
+const credentials = `${organizationID}:${apiToken}`;
+const encodedCredentials = Buffer.from(credentials).toString('base64');
 
-// const config = {
-//   headers: {
-//     'Authorization': `Bearer ${token}`
-//   }
-// };
-
-// axios.get(apiUrl, config)
-//   .then(response => {
-//     console.log('Datos de la respuesta:', response.data);
-//   })
-//   .catch(error => {
-//     console.error('Error al hacer la solicitud:', error);
-//   });
-
-//
-
-const axios = require('axios');
-
-const apiUrl = 'https://app.na5.teamsupport.com/api/json/customers';
-const username = 'publi01@iditic.com';
-const password = 'Z1yXseUE0P';
-const credentials = Buffer.from(`${username}:${password}`).toString('base64');
-const token = '55e32b85-8a30-4533-84a0-9f5b1b81f45f';
-
-const config = {
+const options = {
+  hostname: 'app.teamsupport.com',
+  path: '/api/json/tickets',
+  method: 'GET',
   headers: {
-    'Authorization': `Basic ${credentials}`, // Autenticación básica con las credenciales
-    'X-Token': token // Token adicional
-  }
+    'Authorization': `Basic ${encodedCredentials}`,
+    'Content-Type': 'application/json',
+    'User-Agent': 'Iditic',
+  },
 };
 
-axios.get(apiUrl, config)
-  .then(response => {
-    console.log('Respuesta:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
+
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk; 
   });
+  res.on('end', () => {
+    try {
+      const parsedData = data;
+      console.log('Parsed Response:', parsedData);
+      // Now you can access the data as a JavaScript object
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+    }
+  });
+});
+
+req.on('error', (error) => {
+  console.error('Error:', error);
+});
+
+req.end();
